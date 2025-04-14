ab62753542c3a0155fbbce884acbc38d7a082702
KuaBK <tuanphonglqd@gmail.com>
2025-04-14 23:07:38 +0700

    [Feat]: Init entity again

---
+++ Server/src/main/java/com/example/cua_chat_app/entity/BaseAuditingEntity.java
@@ -0,0 +1,30 @@
+package com.example.cua_chat_app.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseAuditingEntity {
+
+    @CreatedDate
+    @Column(updatable = false, name = "create_date", nullable = false)
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    @Column(name = "last_modified_date", insertable = false)
+    private LocalDateTime lastModifiedDate;
+}
